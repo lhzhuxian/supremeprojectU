de28fb15fe55d7ad5bfa7303039191f39fef2809
Hui Li <huiliyeya@gmail.com>
2018-03-31 13:35:15 -0400

    final edition

--- app/src/main/java/edu/duke/wibbfya/projectU/Chat.java
+++ app/src/main/java/edu/duke/wibbfya/projectU/Chat.java
@@ -14,7 +14,7 @@ import com.squareup.picasso.Picasso;
 
 
 
-public class LapitChat extends Application{
+public class Chat extends Application{
 
     private DatabaseReference mUserDatabase;
     private FirebaseAuth mAuth;
